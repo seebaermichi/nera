f80a556b35a59afafb019235a96141984ea1ec51
Michael Becker <michael.becker@oetkerdigital.com>
2019-11-15 17:06:39 +0100

    Refactor to be more in a functional programming way

---
+++ src/core.js
@@ -0,0 +1,47 @@
+const fs = require('fs')
+const path = require('path')
+
+const fsReaddirRecursive = require('fs-readdir-recursive')
+const Markdown = require('markdown-it')
+const meta = require('markdown-it-meta')
+const md = new Markdown().use(meta)
+const readYaml = require('read-yaml')
+
+const defaultSettings = {
+  folders: {
+    assets: './assets',
+    config: './config',
+    pages: './pages',
+    dist: './public',
+    views: './views'
+  }
+}
+
+const appData = {
+  app: readYaml.sync(`${defaultSettings.folders.config}/app.yaml`),
+  content: null,
+  meta: null,
+  pages: fsReaddirRecursive(defaultSettings.folders.pages),
+  plugins: [],
+  pagesData: []
+}
+
+const getPagesData = pages => {
+  return pages.map(page => ({
+    content: md.render(
+      fs.readFileSync(`${defaultSettings.folders.pages}/${page}`, 'utf-8')
+    ),
+    meta: Object.assign({}, md.meta, {
+      createdAt: fs.statSync(`${defaultSettings.folders.pages}/${page}`)
+        .birthtime,
+      htmlPathName: `/${page.split('.md')[0]}.html`,
+      pagePathName: path.dirname(page)
+    })
+  }))
+}
+
+module.exports = {
+  appData,
+  defaultSettings,
+  getPagesData
+}
---
+++ src/render.js
@@ -0,0 +1,67 @@
+const fs = require('fs')
+const path = require('path')
+
+const ncp = require('ncp').ncp // Asynchronous recursive file & directory copying
+const pug = require('pug')
+const pretty = require('pretty')
+const rimraf = require('rimraf') // The UNIX command rm -rf for node.
+
+const SUCCESS_COLOR = '\x1b[32m%s\x1b[0m'
+
+const copyFolder = (sourceFolder, targetFolder) => {
+  if (fs.existsSync(sourceFolder)) {
+    ncp.limit = 16
+    ncp(sourceFolder, targetFolder, error => {
+      if (error) {
+        return console.log(error)
+      }
+
+      console.log(SUCCESS_COLOR, 'Assets copied')
+    })
+  } else {
+    console.log(SUCCESS_COLOR, 'No Assets found')
+  }
+}
+
+const createHtmlFiles = (data, viewsFolder, publicFolder) => {
+  if (fs.existsSync(viewsFolder)) {
+    data.pagesData.forEach(pageData => {
+      const fn = pug.compileFile(`${viewsFolder}/${pageData.meta.layout}`)
+      const html = fn(Object.assign({}, data, pageData))
+
+      fs.promises
+        .mkdir(path.dirname(`${publicFolder}/${pageData.meta.htmlPathName}`), {
+          recursive: true
+        })
+        .then(() => {
+          fs.writeFileSync(
+            `${publicFolder}/${pageData.meta.htmlPathName}`,
+            pretty(html),
+            'utf-8'
+          )
+
+          console.log(SUCCESS_COLOR, 'Html files created')
+        })
+    })
+  } else {
+    console.error('views folder not found')
+  }
+}
+
+const deleteFolder = folder => {
+  if (fs.existsSync(folder)) {
+    try {
+      rimraf.sync(folder)
+
+      console.log(SUCCESS_COLOR, 'public folder removed')
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+module.exports = {
+  copyFolder,
+  deleteFolder,
+  createHtmlFiles
+}
